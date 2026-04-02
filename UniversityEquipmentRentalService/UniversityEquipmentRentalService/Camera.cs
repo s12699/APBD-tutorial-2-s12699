2a3a698ae89b4f1a132edff040d41bd315d8cf22
s12699 <s12699@pjwstk.edu.pl>
2026-04-02 12:54:25 +0200

    Individual field for each equipment added

--- UniversityEquipmentRentalService/UniversityEquipmentRentalService/Camera.cs
+++ UniversityEquipmentRentalService/UniversityEquipmentRentalService/Camera.cs
@@ -1,6 +1,10 @@
 namespace UniversityEquipmentRentalService;
 
-public class Camera
+public class Camera : Equipment
 {
-    
+    //Information about frames per second
+    public int fps;
+
+    //Information about type of camera (analog, digital)
+    public string type;
 }
