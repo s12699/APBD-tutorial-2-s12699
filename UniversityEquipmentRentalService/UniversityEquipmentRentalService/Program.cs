a0d1cf21e77e220a8f96cc2686e498096c272df4
s12699 <s12699@pjwstk.edu.pl>
2026-04-02 12:59:24 +0200

    adding body to the main program class

--- UniversityEquipmentRentalService/UniversityEquipmentRentalService/Program.cs
+++ UniversityEquipmentRentalService/UniversityEquipmentRentalService/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace UniversityEquipmentRentalService;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
